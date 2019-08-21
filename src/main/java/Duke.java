6e60aec3f19d71ae38659cd36e473b5f12ecb3ea
Pdotdeep <paulroopson.pradeep@gmail.com>
2019-08-21 20:47:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    static void printLine(){
+        System.out.println("\t_______________________________________");
+    }
+    static void echo(String str){
+        System.out.println("\t"  + str);
+    }
+    static void response(String s){
+        printLine();
+        echo(s);
+        printLine();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        printLine();
+        echo("Hello! I am Duke");
+        echo("What can i Do for you?");
+        printLine();
+        ArrayList<String> items = new ArrayList<String>();
+        while(true){
+            String s = scan.nextLine();
+            if(s.equals("bye")){
+                response("Bye. Hope to see you again Soon!");
+                break;
+            }
+//            else if(s.equals("list")){
+//                System.out.println(items);
+//            }
+            response(s);
+        }
+
     }
 }
